858e7861f3be9a15821ca66f2c0f9f4fe5be0d66
Mehebul02 <mehebul2123@gmail.com>
2024-09-29 21:54:52 +0600

    twmarge added

---
+++ src/shared/Header.jsx
@@ -0,0 +1,11 @@
+import Container from "./Container";
+
+const Header = () => {
+    return (
+        <Container className="mt-6">
+            <h1 className="text-red-500">My Name is Mehebul Alif</h1>
+        </Container>
+    );
+};
+
+export default Header;
